1510a5d4226c0a790cc59a2105fc1599524e5a98
Brian Wilson (DrSpeedy) <bwilson3751@outlook.com>
2018-02-10 02:38:43 -0500

    Somewhat working prototype. Bios booting lvm works on usb. X does not start correctly

--- build.sh
+++ build.sh
@@ -29,20 +29,21 @@ pacman_conf=${work_dir}/pacman.conf
 
 # Installation profile
 install_profile="set-top-box"
-profile_dir="${script_dir}/profile/${install_profile}"
+profile_dir="${script_dir}/profiles/${install_profile}"
 
 
 # Source in utility scripts
-source "${script_dir}/resources/util.sh"
-source "${script_dir}/resources/configure.sh"
+source "${script_dir}/resources/alias.rc"
+source "${script_dir}/resources/util.rc"
+source "${script_dir}/resources/configure.rc"
 
 # Run configuration scripts to build partition
 # table and set other system install variables
 configure_system() {
     _msg_info "Running configuration..."
     
-    _select_disk
-    _select_partition_scheme
+    __select_disk
+    __select_partition_scheme
     #_select_hostname
     #_select_timezone
 }
@@ -50,14 +51,15 @@ configure_system() {
 # Initialize the build enviornment
 initialize() {
     _msg_info "Initializing build environment..."
+    mkdir -p ${work_dir}
     mkdir -p ${build_dir}
 
-    _msg_info "Generating pacman.conf -> ${pacman_conf}..."
+    _msg_info "Generating pacman.conf..."
+    _msg_info "${script_dir}/pacman.conf -> ${pacman_conf}"
 
     local _cache_dirs
     _cache_dirs=($(pacman -v 2>&1 | grep '^Cache Dirs:' | sed 's/Cache Dirs:\s*//g'))
-    sed -r "s|^#?\\s*CacheDir.+|CacheDir = $(echo -n ${_cache_dirs[@]})|g"\ 
-        ${script_dir}/pacman.conf > ${pacman_conf}
+    sed -r "s|^#?\\s*CacheDir.+|CacheDir = $(echo -n ${_cache_dirs[@]})|g" ${script_dir}/pacman.conf > ${pacman_conf}
 
     # Run pre-build profile hook if it exists
     if [[ -f ${profile_dir}/hooks/pre-build.hook ]]; then
@@ -68,38 +70,69 @@ initialize() {
 
 make_basefs() {
     _msg_info "Building basefs..."
-    local _pkgs=(base base-devel git zsh)
+    local _pkgs=(base base-devel git zsh grub)
     local _adtl_pkgs=$(grep -h -v ^# ${script_dir}/pkglist.{any,$arch})
-    setarch ${arch} _pacman ${_pkgs[@]} ${_adtl_pkgs[@]}
+    _pacman ${_pkgs[@]} ${_adtl_pkgs[@]}
+
+    if [[ $? -ne 0 ]]; then
+        _msg_error "Package installation failed!" 2
+    fi
+
+    # EFI tools
+    if [[ ${use_uefi} ]]; then
+        _msg_info "EFI detected! Installing additional packages..."
+        local _efi_pkgs=(efibootmgr)
+        _pacman ${_efi_pkgs}
+
+        if [[ $? -ne 0 ]]; then
+            _msg_error "EFI tools installation failed!" 2
+        fi
+    fi
+    
 
     _msg_info "Syncing airootfs..."
     _rsync ${script_dir}/airootfs/* ${build_dir}
 
     _msg_info "Running base provisioning script..."
-    _chroot_run "exec /root/base.sh"
+    _chroot_run "/root/base.sh"
 }
 
 make_profile() {
     # Double line break for easier reading of output
     echo -e "\n\n"
-    _msg_info "Building ${profile}..."
+    _msg_info "Building ${install_profile}..."
+    _msg_info "${profile_dir}"
     
-    if [[ -d ${profile_dir} && ${install_profile} -ne "base" ]]; then
+    if [[ -d ${profile_dir} ]]; then
+        local _pkgs=$(grep -h -v ^# ${profile_dir}/pkglist.{any,$arch})
+        _pacman ${_pkgs[@]}
+
+        _rsync ${profile_dir}/airootfs/* ${build_dir}
         _rsync ${profile_dir}/${install_profile}.sh ${build_dir}/root
 
         _msg_info "Running ${install_profile} provisioning script..."
-        _chroot_run "exec /root/${install_profile}.sh"
+        _chroot_run "/root/${install_profile}.sh"
         _chroot_run "rm /root/${install_profile}.sh"
 
-        #TODO: Post install hook
     else
         _msg_error "Profile ${profile} not found!" 3
     fi
 }
 
+make_grub_bootloader() {
+    if [[ ${use_uefi} -eq 0 ]]; then
+        _msg_info "Installing GRUB..."
 
+        _rsync ${script_dir}/grub/grub.cfg ${build_dir}/etc/default/grub
+        _chroot_run "grub-mkconfig -o /boot/grub/grub.cfg"
 
-_run_once configure_system
+        grub-install --target=i386-pc ${install_disk}
+    fi
+}
+
 _run_once initialize
+_run_once make_pacman_config
+_run_once configure_system
 _run_once make_basefs
 _run_once make_profile
+_run_once make_grub_bootloader
--- profiles/set-top-box/set-top-box.sh
+++ profiles/set-top-box/set-top-box.sh
@@ -20,6 +20,15 @@ chmod 700 /root
 
 sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
 
+# Update mirror list
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+reflector --country 'United States' --latest 100 --age 24 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+# Initialize pacman and populate the keyring
+pacman-key --init
+pacman-key --populate archlinux
+
+systemctl set-default multi-user.target
 systemctl enable NetworkManager.service
 systemctl enable bluetooth.service
 systemctl enable nginx.service
@@ -29,6 +38,11 @@ systemctl enable stb.service
 groupadd media
 useradd -m -g media -G "users,lp,audio,video" -s /bin/zsh media
 
-useradd -m -g users -G "wheel,lp,audio,video" -s /bin/zsh doc
+useradd -m -p "password" -g users -G "wheel,lp,audio,video" -s /bin/zsh doc
 echo -e "Set password for <doc>"
-passwd doc
+#passwd doc
+
+genfstab -L / >> /etc/fstab
+
+echo "STB-$$" > /etc/hostname
+
